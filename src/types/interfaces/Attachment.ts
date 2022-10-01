13143aab93ebf15dae5c4f00fdce0b752d5c7856
pchchv <ipchchv@gmail.com>
2022-10-02 02:54:16 +0300

    Implement a attachment interface

---
+++ src/types/interfaces/Attachment.ts
@@ -0,0 +1,9 @@
+import { ATTACHMENT_DISPOSITION } from "@/types/enums/attachment_disposition"
+import { ATTACHMENT_MIME_TYPE } from "@/types/enums/attachment_mime_type"
+
+export interface Attachment {
+    content: string
+    type?: ATTACHMENT_MIME_TYPE
+    filename: string
+    disposition?: ATTACHMENT_DISPOSITION
+}
