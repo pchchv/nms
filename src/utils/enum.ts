4331bda36c6fe3b812dae8adcb0b8e9fe434c279
pchchv <ipchchv@gmail.com>
2022-10-03 19:23:32 +0300

    Implement a configuration schema

---
+++ src/utils/enum.ts
@@ -0,0 +1,9 @@
+const list = ( enm: Record<string,unknown> ): string[] => {
+    const values = [] as string[]
+    for(const key in enm) {
+        values.push(enm[key] as string)
+    }
+    return values
+}
+
+export { list }
