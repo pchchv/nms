a7e785bfc918d45cb84cd5e832ae5d1e8db486ef
pchchv <ipchchv@gmail.com>
2022-10-03 19:28:42 +0300

    Implement a debug decorator

---
+++ src/types/decorators/debug.ts
@@ -0,0 +1,88 @@
+import { Container } from '@/services/container'
+import { TRANSPORTER } from '@/types/enums/transporter'
+
+const Debug = ( transporter: string ): any => {
+    return ( target: Record<string,({...args}: any)=>any>, key: string ) => {
+        const method = target[key] as ({...args}: any) => any
+        target[key] = function (...args: any[]): any {
+            const output = method.apply(this, args) as {
+                Message?: {
+                    From: {
+                        Email: string,
+                        Name: string
+                    }
+                },
+                Messages?: Array<{From: { Email: string, Name: string}}>,
+                from?: string,
+                to?: Array<string>,
+                cc?: Array<string>,
+                bcc?: Array<string>,
+                html?: string,
+                text?: string,
+                options?: { sandbox: boolean },
+                content?: {
+                    from?: string
+                }
+            }
+            if (output) {
+                if (Container.configuration?.sandbox?.active) {
+                    switch(transporter) {
+                        case TRANSPORTER.mailgun:
+                            Object.assign(output, { testmode: true })
+                            break
+                            case TRANSPORTER.mailjet:
+                                Object.assign(output, {
+                                    SandboxMode: true,
+                                    mail_settings: {
+                                        sandbox_mode: {
+                                            enable: true
+                                        }
+                                    }
+                                })
+                                output.Messages[0].From = { Email: Container.configuration.sandbox.from.email, Name: Container.configuration.sandbox.from.name }
+                                break
+                                case TRANSPORTER.postmark:
+                                    output.from = `${Container.configuration.sandbox.from.name} ${Container.configuration.sandbox.from.email}`
+                                    output.to = [].concat(output.to).map( (recipient) => {
+                                        return `${Container.configuration.sandbox.to.name} ${Container.configuration.sandbox.to.email}`
+                                    })
+                                    if(output.cc) {
+                                        output.cc = [].concat(output.cc).map( (recipient) => {
+                                            return `${Container.configuration.sandbox.to.name} ${Container.configuration.sandbox.to.email}`
+                                        })
+                                    }
+                                    if(output.bcc) {
+                                        output.bcc = [].concat(output.bcc).map( (recipient) => {
+                                            return `${Container.configuration.sandbox.to.name} ${Container.configuration.sandbox.to.email}`
+                                        })
+                                    }
+                                    break
+                                    case TRANSPORTER.sendgrid:
+                                        Object.assign(output, {
+                                            mail_settings: {
+                                                sandbox_mode: {
+                                                    enable: true
+                                                }
+                                            }
+                                        })
+                                        break
+                                        case TRANSPORTER.sparkpost:
+                                            Object.assign(output, {
+                                                options: {
+                                                    sandbox: true
+                                                },
+                                                content: {
+                                                    from: Container.configuration.sandbox.to.email
+                                                }
+                                            })
+                                            break
+                    }
+                }
+                return output
+            }
+        }
+        return target[key] as ({...args}) => any
+    }
+}
+
+export { Debug }
