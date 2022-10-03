55a1fbbd669def6764200acc277967f92c06cd89
pchchv <ipchchv@gmail.com>
2022-10-03 18:06:03 +0300

    Add TRANSPORTER enum

---
+++ src/types/enums/transporter.ts
@@ -0,0 +1,10 @@
+export enum TRANSPORTER {
+    sparkpost = 'sparkpost',
+    sendgrid = 'sendgrid',
+    sendinblue = 'sendinblue',
+    mandrill = 'mandrill',
+    mailgun = 'mailgun',
+    mailjet = 'mailjet',
+    postmark = 'postmark',
+    smtp = 'smtp'
+}
