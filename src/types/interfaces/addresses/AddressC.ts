d4df668a5508ba57ec45b51722c57590f3f9881b
pchchv <ipchchv@gmail.com>
2022-10-05 15:49:53 +0300

    Add types

---
+++ src/types/interfaces/addresses/AddressC.ts
@@ -0,0 +1,4 @@
+import { AddressA } from '@/types/interfaces/addresses/AddressA'
+import { AddressB } from '@/types/interfaces/addresses/AddressB'
+
+export interface IAddressC extends AddressA, AddressB {}
